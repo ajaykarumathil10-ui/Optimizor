40bdea14ee0c3c7ab3ab0501f276b7414869a4bc
Ajay Karumathil <ajaykarumathil10@gmail.com>
2026-01-18 12:17:31 +0530

    developer about box

---
+++ Optimizor/AboutDeveloper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Optimizor
+{
+  partial class AboutDeveloper : Form
+  {
+    public AboutDeveloper()
+    {
+      InitializeComponent();
+      this.Text = String.Format("About {0}", AssemblyTitle);
+      this.labelDevName.Font = new Font(labelDevName.Font, FontStyle.Bold);
+      this.labelDevName.Text = "Ajay Karumathi";
+      this.labelCompanyName.Text = " Works at Infosys Ltd";
+      this.textBoxDescription.Text = "Mid-level .NET Core Full Stack Developer with almost 5 years hands-on experience specializing in " +
+      "microservices architecture and RESTful APIs. Proven expertise in building scalable web Applications systems using .NET Core, MSSQL," +
+      " Angular and React frontend integration. Strong in system design, performance optimization, & event-driven patterns. ";
+    }
+
+    #region Assembly Attribute Accessors
+
+    public string AssemblyTitle
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+        if (attributes.Length > 0)
+        {
+          AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+          if (titleAttribute.Title != "")
+          {
+            return titleAttribute.Title;
+          }
+        }
+        return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+      }
+    }
+
+    public string AssemblyVersion
+    {
+      get
+      {
+        return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+      }
+    }
+
+    public string AssemblyDescription
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+      }
+    }
+
+    public string AssemblyProduct
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyProductAttribute)attributes[0]).Product;
+      }
+    }
+
+    public string AssemblyCopyright
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+      }
+    }
+
+    public string AssemblyCompany
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyCompanyAttribute)attributes[0]).Company;
+      }
+    }
+    #endregion
+  }
+}
